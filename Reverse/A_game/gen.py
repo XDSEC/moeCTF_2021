ae3348b0b076872e51246c970881658ef39a1298
traLk <71552622+traLk@users.noreply.github.com>
2021-09-21 14:30:41 +0800

    Delete gen.py

--- Reverse/A_game/gen.py
+++
@@ -1,4 +0,0 @@
-str1 = "8291767138932581849755263447186268341129653538127"
-str2 = "S0_As_I_prAy_Un1imited_B1ade_WOrks---E1m1ya_Shiro"
-for i in range(49):
-    print(hex(ord(str1[i])^ord(str2[i])),end=",")
